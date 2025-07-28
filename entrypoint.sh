ad5a6f39d31ab11b52b487e12b737f247e37e84b
Rishabh Rastogi <rrastogi@avataracloud.com>
2025-07-28 17:48:25 +0530

    ssh config for azure

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Start SSH service
+echo "Starting SSH service..."
+/usr/sbin/sshd
+
 # Run your existing database initialization command
 echo "Running Giani PKB database initialization..."
 python -m giani_pkb.database.database_initialize
